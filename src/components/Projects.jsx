d719b837d2d56f675484aa332b9551fa16d6c5c8
Sami Uysal <42553892+Sami-Red@users.noreply.github.com>
2026-07-25 17:39:57 +0100

    projects
    Begun creating the project's section of the site and small tuning to about me section

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,40 +4,29 @@ function Projects(){
     return(
     <div className="projects">
         <div className="projects_top">
-            <h1>Projects</h1>
+            <h1>Featured Projects</h1>
         </div>
-
+        
         <div className="projects_bottom">
+            <div className="projects_card">
 
-            <div className="project_card">
-
-                <img />
-
-                <h2>Portfolio Website</h2>
-
-                <p>
-                    Personal portfolio built with
-                    React and Vite.
-                </p>
-
-                <div className="project_stack">
-
-                    <span>React</span>
-                    <span>Vite</span>
-                    <span>CSS</span>
-
+                <div className="project_image">
                 </div>
 
-                <div className="project_buttons">
-
-                    <a>GitHub</a>
-                    <a>Live Demo</a>
-
+                <div className="project_content">
+                    <p>abdf</p>
                 </div>
-
             </div>
+            <div className="projects_card">
 
-        </div>
+                <div className="project_image">
+                </div>
+
+                <div className="project_content">
+                    <p>abdf</p>
+                </div>
+            </div>
+        </div>        
     </div>
     )
 }
