ccd561ea98c725ea53d0cc4a0dab8d246fe2d283
Sami Uysal <42553892+Sami-Red@users.noreply.github.com>
2026-08-08 09:15:15 +0100

    added experience
    created and added the experience page

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ function App() {
       <About/>
       <Projects/>
       <Skills/>
+      <Experience/>
     </div>
   )
   
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,8 +1,45 @@
-function Experience(){
-    return(
-    <div className="experience">
-        <p>skills</p>
-    </div>
-    )
+import "./Experience.css";
+
+function Experience() {
+    return (
+        <div className="experience">
+
+            <div className="experience_top">
+                <h1>Experience</h1>
+            </div>
+
+            <div className="experience_timeline">
+
+                <div className="timeline_item">
+                    <div className="timeline_marker"></div>
+
+                    <div className="timeline_content">
+                        <h2>B Bagel</h2>
+                        <h3>Supervisor • 2025 – Present</h3>
+                        <p>
+                            Supported food production, store operations, and efficient
+                            team coordination.
+                        </p>
+                    </div>
+                </div>
+
+                <div className="timeline_item">
+                    <div className="timeline_marker"></div>
+
+                    <div className="timeline_content">
+                        <h2>Chipotle</h2>
+                        <h3>Supervisor & Trainer • 2022 – 2025</h3>
+                        <p>
+                            Managed shifts, trained staff, and maintained operational
+                            standards.
+                        </p>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    );
 }
+
 export default Experience;
